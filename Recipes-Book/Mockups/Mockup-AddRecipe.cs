60af0d95aef33d9adb16f4dcc1ac5ab7ffd76044
Sandru Gabriel <gabi200611@yahoo.com>
2023-02-09 19:48:54 +0200

    done

---
+++ Recipes-Book/Mockups/Mockup-AddRecipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recipes_Book.Mockups
+{
+    public partial class Mockup_AddRecipe : Form
+    {
+        public Mockup_AddRecipe()
+        {
+            InitializeComponent();
+        }
+    }
+}

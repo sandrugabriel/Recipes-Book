583ca146d3b94efb88b0365c7da48536ed02443d
Sandru Gabriel <gabi200611@yahoo.com>
2023-02-09 19:14:12 +0200

    done

---
+++ Recipes-Book/Models/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes_Book.Models
+{
+    internal class Client
+    {
+
+        private int id;
+        private string name;
+        private string password;
+        private string recipes;
+
+        public Client(int id, string name, string password, string recipes)
+        {
+
+            this.id = id;
+            this.name = name;
+            this.password = password;
+            this.recipes = recipes;
+
+        }
+
+        public int getId() { 
+            return id; }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+
+        public string getRecipes()
+        {
+           return recipes;
+        }
+
+    }
+}

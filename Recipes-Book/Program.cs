c95d47ee9c19b2ab969d20f879966daf1beaebfb
Sandru Gabriel <gabi200611@yahoo.com>
2023-02-25 18:36:52 +0200

    done

--- Recipes-Book/Program.cs
+++ Recipes-Book/Program.cs
@@ -18,7 +18,7 @@ namespace Recipes_Book
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1(1));
+            Application.Run(new FormLogin());
 
         }
     }
